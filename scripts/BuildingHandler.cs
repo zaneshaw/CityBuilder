be1b341ff30dd883206b6cfd1a2dd09fa309fdff
Zane Shaw <admin@triangle100.com>
2023-03-12 22:03:00 +1100

    Add temporary tile highlight

--- scripts/BuildingHandler.cs
+++ scripts/BuildingHandler.cs
@@ -2,19 +2,27 @@ using Godot;
 using System;
 
 public partial class BuildingHandler : TileMap {
+	private Vector2I tilePos;
+
 	public override void _Ready() {
 		
 	}
 
 	public override void _Process(double delta) {
+		Vector2I newTilePos = LocalToMap(GetLocalMousePosition());
+
+		if (newTilePos != tilePos) {
+			SetCell(2, tilePos, -1);
+			tilePos = newTilePos;
+			SetCell(2, tilePos, 2, new Vector2I(0, 0));
+		}
+
 		if (Input.IsActionJustPressed("SecondaryInteract")) {
-			Vector2I tilePos = LocalToMap(GetLocalMousePosition());
 			SetCell(1, tilePos, -1);
 			GD.Print(tilePos);
 		}
 
 		if (Input.IsActionJustPressed("PrimaryInteract")) {
-			Vector2I tilePos = LocalToMap(GetLocalMousePosition());
 			SetCell(1, tilePos, 1, new Vector2I(1, 0));
 			GD.Print(tilePos);
 		}
