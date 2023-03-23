295a10411144c863ac2f5cf0c44aa0f38ae801d9
Zane Shaw <admin@triangle100.com>
2023-03-23 22:46:17 +1100

    Fix camera panning

--- scripts/CameraController.cs
+++ scripts/CameraController.cs
@@ -1,27 +1,23 @@
 using Godot;
 
 public partial class CameraController : Camera2D {
-	[Export] float zoomSpeed = 0.05f;
-	Vector2 mouseOld;
+    [Export] float zoomSpeed = 0.05f;
+    Vector2 mouseOld;
 
     public override void _Process(double delta) {
-		Vector2 mouseDelta = GetViewport().GetMousePosition() - mouseOld;
-		mouseOld += mouseDelta;
+        Vector2 mouseDelta = GetViewport().GetMousePosition() - mouseOld;
+        mouseOld += mouseDelta;
 
-		if (Input.IsActionPressed("CameraPan")) {
-			Position -= mouseDelta;
-		}
+        if (Input.IsActionPressed("CameraPan")) {
+            Position -= mouseDelta / Zoom;
+        }
 
-		if (Input.IsActionJustReleased("CameraZoomIn")) {
-			GD.Print(Zoom);
-			Zoom = new Vector2(Zoom.X + zoomSpeed, Zoom.Y + zoomSpeed);
-			GD.Print(Zoom);
-		}
+        if (Input.IsActionJustReleased("CameraZoomIn")) {
+            Zoom = new Vector2(Zoom.X + zoomSpeed, Zoom.Y + zoomSpeed);
+        }
 
-		if (Input.IsActionJustReleased("CameraZoomOut")) {
-			GD.Print(Zoom);
-			Zoom = new Vector2(Zoom.X - zoomSpeed, Zoom.Y - zoomSpeed);
-			GD.Print(Zoom);
-		}
+        if (Input.IsActionJustReleased("CameraZoomOut")) {
+            Zoom = new Vector2(Zoom.X - zoomSpeed, Zoom.Y - zoomSpeed);
+        }
     }
 }
