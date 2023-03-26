84e237c33c3e4d58db9140ce9b537b10f2d43437
Zane Shaw <admin@triangle100.com>
2023-03-26 22:13:12 +1100

    Add procedural generation

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -5,11 +5,13 @@ using UnityEngine.Tilemaps;
 
 public class TerrainGenerator : MonoBehaviour {
     [SerializeField] private Vector2Int mapSize;
-    [SerializeField] private Vector2Int devTile;
 
     [SerializeField] private Tilemap terrainTilemap;
-    [SerializeField] private TileBase grassTile;
-    [SerializeField] private TileBase waterTile;
+    [SerializeField] private List<TileBase> tiles;
+
+    [SerializeField] private Gradient gradientTest;
+    [SerializeField] private float scale = 1f;
+    [SerializeField] private int heightScale = 2;
 
     [ContextMenu("Generate Map")]
     public void GenerateMap() {
@@ -17,8 +19,14 @@ public class TerrainGenerator : MonoBehaviour {
 
         for (int y = 0 - mapSize.y / 2; y < mapSize.y / 2f; y++) {
             for (int x = 0 - mapSize.x / 2; x < mapSize.x / 2f; x++) {
+                float xV2 = x + mapSize.x / 2f;
+                float yV2 = y + mapSize.y / 2f;
+                float noiseValue = Mathf.PerlinNoise(0f + (float)xV2 / mapSize.x * scale, 0f + (float)yV2 / mapSize.y * scale);
 
-                terrainTilemap.SetTile(new Vector3Int(x, y), x == devTile.x && y == devTile.y ? waterTile : grassTile);
+                int spriteIndex = Mathf.RoundToInt((gradientTest.Evaluate(noiseValue).r * 255f) / 10f);
+                int elevation = Mathf.RoundToInt((gradientTest.Evaluate(noiseValue).g * 255f) / 10f);
+
+                terrainTilemap.SetTile(new Vector3Int(x, y, elevation * heightScale), tiles[spriteIndex]);
             }
         }
     }
