eb9ba83876ee06325f44e855eea358206e1235ec
Zane Shaw <admin@triangle100.com>
2023-03-27 22:36:32 +1100

    Add basic camera zooming

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
+    [SerializeField] private float zoomSpeed = 1f;
+
     private Vector3 panOrigin;
     private bool panning;
 
-    private void LateUpdate() {
+    private void Update() {
         if (Input.GetMouseButton(2)) {
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector3 mouseDelta = mousePosition - transform.position;
@@ -19,6 +21,10 @@ public class CameraController : MonoBehaviour {
             transform.position = panOrigin - mouseDelta;
         } else {
             panning = false;
+        }
+
+        if (Input.GetAxisRaw("Mouse ScrollWheel") != 0) {
+            Camera.main.orthographicSize -= Input.mouseScrollDelta.y * zoomSpeed;
         }
     }
 }
